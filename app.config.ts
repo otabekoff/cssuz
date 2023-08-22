0f3200f0be2dde4959b8ec515a6ffbba8156ba48
Otabek <60849894+otabekoff@users.noreply.github.com>
2023-08-22 13:33:01 +0500

    Ilk o'zgarishlar
    Ba'zi bir kichik o'zgarishlar amalga oshirildi.

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,9 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'CSS - O\'zbek tilida',
+    description: 'CSS veb-texnologiyasini o\'rganish uchun eng yaxshi joy.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
@@ -12,6 +13,7 @@ export default defineAppConfig({
         href: 'https://nuxt.com'
       }
     },
+
     github: {
       dir: '.starters/default/content',
       branch: 'main',
@@ -19,20 +21,35 @@ export default defineAppConfig({
       owner: 'nuxt-themes',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
+      fluid: true,
+      title: 'CSS - O\'zbek tilida'
+    },
+
+    footer: {
+      credits: {
+        text: 'Created by Otabek Sadiridinov',
+        href: 'https://css.nuxt.space'
+      },
+
       fluid: true
-    }
+    },
+
+    titleTemplate: '%s · CSS (O\'zbek tilida)'
   }
 })
